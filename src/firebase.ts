744ae44763f005998504cba8533f50fe2d581ca7
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-10-05 01:32:49 +0530

    Add: Firebase

---
+++ src/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBW9EHb8qgq8G61O4fMYcDBVXqYf_wSyac",
+  authDomain: "linkgrid-71092.firebaseapp.com",
+  projectId: "linkgrid-71092",
+  storageBucket: "linkgrid-71092.appspot.com",
+  messagingSenderId: "848389050209",
+  appId: "1:848389050209:web:b7d964cf0083519295bf20",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { auth };
+export default app;

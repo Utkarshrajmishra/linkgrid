7b89433b271b0ea2e6cf272e5f8876d428d14538
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-10-05 01:27:56 +0530

    Add: Auth Page

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,8 +1,10 @@
 import Login from "@/components/Login";
+import Signup from "@/components/Signup";
 import Logo from "../assets/image/logo.png";
+import { useState } from "react";
 
 const Auth = () => {
-
+    const [signUp, setsignUp] = useState(true);
   return (
     <div className="flex min-h-screen">
       <section className="w-full md:w-1/2 p-4 md:p-6 flex flex-col">
@@ -13,7 +15,7 @@ const Auth = () => {
           <img src={Logo} alt="" className="w-5" />
         </div>
         <div className="flex-1 flex items-center justify-center">
-          <Login />
+          {signUp ? <Signup /> : <Login />}
         </div>
       </section>
       <div className="bg-gradient-to-tr from-green-500 to-teal-500 hidden md:block md:w-1/2"></div>

2b4d25e4cedd014f4ffa74cbbfe5e63ecf0d4a0a
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-10-10 01:50:28 +0530

    Add: Progress Component

--- src/components/Mobile.tsx
+++ src/components/Mobile.tsx
@@ -11,32 +11,34 @@ import { Templates } from "@/constants/Template";
 const Mobile = () => {
   return (
     <div
-      className={`flex h-[100vh] w-[100%] md:w-[375px] justify-center min-h-screen p-4 ${Templates.light.bgColor}`}
+      className={`flex h-[100vh] w-[100%]  md:w-[100vw] justify-center min-h-screen p-4  ${Templates.green.bgColor}`}
     >
       <div
-        className={`w-full ${Templates.light.textColor}  overflow-x-hidden gap-4 flex-col flex items-center `}
+        className={`w-full ${Templates.green.textColor} md:w-[55%]  overflow-x-hidden gap-4 flex-col flex items-center `}
       >
         <div className="flex flex-col items-center gap-2">
           <img src={Profile} alt="" className="w-20 h-20 mt-6" />
           <div className="flex flex-col items-center">
-            <p className="text-xl font-inter font-semibold">Olivia Wilde</p>
+            <p className="text-xl md:text-3xl font-inter font-semibold">
+              Olivia Wilde
+            </p>
             <p className="font-inter text-sm md:text-md">
               Happiness never goes out of style
             </p>
           </div>
         </div>
-        <div className="flex gap-4 mt-2 justify-center">
-          <FaInstagram color={Templates.light.iconColor} fontSize={22} />
-          <FaWhatsapp color={Templates.light.iconColor} fontSize={22} />
-          <FaTiktok color={Templates.light.iconColor} fontSize={22} />
-          <FaFacebook color={Templates.light.iconColor} fontSize={22} />
-          <FaSnapchatGhost color={Templates.light.iconColor} fontSize={22} />
+        <div className="flex gap-4 md:gap-6 mt-2 justify-center">
+          <FaInstagram className="text-[25px] text-[Templates.green.iconColor]  md:text-[30px]" />
+          <FaWhatsapp className="text-[25px] text-[Templates.green.iconColor] md:text-[30px]" />
+          <FaTiktok className="text-[25px] text-[Templates.green.iconColor]  md:text-[30px]" />
+          <FaFacebook className="text-[25px] text-[Templates.green.iconColor]  md:text-[30px]" />
+          <FaSnapchatGhost className="text-[25px] text-[Templates.green.iconColor]  md:text-[30px]" />
         </div>
         <div
-          className={`flex font-inter mt-2 flex-col items-center ${Templates.light.textColor} gap-4 w-full  overflow-scroll p-1`}
+          className={`flex font-inter mt-2 flex-col items-center ${Templates.green.textColor} gap-4 w-full  overflow-scroll p-1`}
         >
           <div
-            className={`text-sm ${Templates.light.shadow} ${Templates.light.outline} ${Templates.light.containerBgColor} cursor-pointer  p-[10px] rounded-2xl flex justify-center w-full`}
+            className={`text-sm ${Templates.green.shadow} ${Templates.green.outline} ${Templates.green.containerBgColor} cursor-pointer  p-[12px] rounded-2xl flex justify-center w-full`}
           >
             <p>Latest Youtube Video</p>
           </div>
--- src/pages/Template.tsx
+++ src/pages/Template.tsx
@@ -1,13 +1,17 @@
-import Mobile from "@/components/Mobile";
-
+import Progress from "@/components/Progress";
 const Template = () => {
   return (
     <>
-      <section className="flex gap-2 items-center flex-wrap justify-center">
-        <Mobile />
-        
-        
-      </section>
+      <div className="flex justify-center min-h-screen">
+        <section className="md:w-[60%] mt-10  w-full items-center p-4 md:p-6 flex flex-col gap-8">
+          <Progress/>
+
+          <h1 className="text-5xl font-[900] font-inter">Select a Template</h1>
+          <p className="font-inter text-zinc-500">
+            Pick the style that feels right - you can add your content later
+          </p>
+        </section>
+      </div>
     </>
   );
 };

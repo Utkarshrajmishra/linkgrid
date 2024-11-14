491bcd0932b126758257bff6cc62d9643d5e1b20
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-11-14 19:26:40 +0530

    Add: Auth Context

---
+++ src/context/Auth.tsx
@@ -0,0 +1,26 @@
+import { createContext, ReactNode, useState } from "react";
+import { AuthContextTypes } from "@/types/Types";
+
+export const AuthContext = createContext<AuthContextTypes>({
+  auth: {
+    isLogin: false,
+    userEmail: "",
+  },
+  setAuth: () => {},
+});
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const [auth, setAuth] = useState({
+    isLogin: false,
+    userEmail: "",
+  });
+
+  const contextValue: AuthContextTypes = {
+    auth,
+    setAuth,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
